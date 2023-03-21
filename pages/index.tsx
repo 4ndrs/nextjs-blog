231c4f2db3f142028e8e4412d29328e4ee65ffef
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-21 22:58:49 +0100

    Add up to `Link Component` modifications

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import Head from "next/head";
+import Link from "next/link";
 
-export default function Home() {
+import styles from "../styles/Home.module.css";
+
+const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +13,7 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href="/posts/first-post">this post!</Link>
         </h1>
 
         <p className={styles.description}>
@@ -55,7 +57,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
         </a>
       </footer>
@@ -111,5 +113,7 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
