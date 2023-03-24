ba732953514c9bd9de00add53008af8ec8661608
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-24 23:44:47 +0100

    Add up to `API Routes Details` modifications

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
 import Head from "next/head";
+import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
 
+import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
 export const getStaticProps = async () => {
@@ -37,11 +39,11 @@ const Home = ({ allPostsData }: HomeProps) => (
       <ul className={utilStyles.list}>
         {allPostsData.map(({ id, date, title }) => (
           <li className={utilStyles.listItem} key={id}>
-            {title}
+            <Link href={`/posts/${id}`}>{title}</Link>
             <br />
-            {id}
-            <br />
-            {date}
+            <small className={utilStyles.lightText}>
+              <Date dateString={date} />
+            </small>
           </li>
         ))}
       </ul>
