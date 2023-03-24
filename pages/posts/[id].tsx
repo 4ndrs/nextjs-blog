29f40dbd75608397a41f8894f7c15195099a1f08
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2023-03-24 17:35:13 +0100

    Add `Polishing the Post Page` modifications

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -1,5 +1,8 @@
+import Head from "next/head";
+import Date from "../../components/date";
 import Layout from "../../components/layout";
 
+import utilStyles from "../../styles/utils.module.css";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 type Path = ReturnType<typeof getAllPostIds>[number];
@@ -27,12 +30,19 @@ type PostProps = { postData: Awaited<ReturnType<typeof getPostData>> };
 
 const Post = ({ postData }: PostProps) => (
   <Layout>
-    {postData.title}
-    <br />
-    {postData.id}
-    <br />
-    {postData.date}
-    <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    <Head>
+      <title>{postData.title}</title>
+    </Head>
+
+    <article>
+      <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+
+      <div className={utilStyles.lightText}>
+        <Date dateString={postData.date} />
+      </div>
+
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </article>
   </Layout>
 );
 
